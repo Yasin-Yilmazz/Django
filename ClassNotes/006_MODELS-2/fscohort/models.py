da235058333520bcce3cd8754ca8ca96e17bd6d6
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-08-25 16:29:47 +0300

    lessen half end

--- ClassNotes/006_MODELS-2/fscohort/models.py
+++ ClassNotes/006_MODELS-2/fscohort/models.py
@@ -1,2 +1,14 @@
+from tabnanny import verbose
 from django.db import models
 
+class Student(models.Model):
+    first_name = models.CharField("Name", max_length=50)
+    last_name = models.CharField("LastName", max_length=50)
+    number = models.IntegerField("Number")
+
+    def __str__(self):
+        return f"{self.number} - {self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["number"] #DESC --> ["-number"]
+        verbose_name_plural = "Students"
