0fb97705d85ceb70c2cb78c958e0f09c257c6a85
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-08-28 11:52:31 +0300

    cb

--- ClassNotes/010_Class_Based_Views/fscohort/urls.py
+++ ClassNotes/010_Class_Based_Views/fscohort/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import home,student_list, student_add, student_detail, student_update,student_delete
+from .views import HomeView, StudentListView, StudentDetailView
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('student_list/', student_list, name="list"),
+    # path('', home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    # path('student_list/', student_list, name="list"),
+    path('student_list/', StudentListView.as_view(), name="list"),
     path('student_add/', student_add, name="add"),
-    path('detail/<int:id>/', student_detail, name="detail"),
+    # path('detail/<int:id>/', student_detail, name="detail"),
+    path('detail/<int:id>/', StudentDetailView.as_view(), name="detail"),
     path('update/<int:id>/', student_update, name="update"),
     path('delete/<int:id>/', student_delete, name="delete"),
 ]
