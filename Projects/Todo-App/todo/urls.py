01b0300e51fbf1ccb52f9d947a46603ce550bc29
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-08-30 10:10:41 +0300

    todo last

--- Projects/Todo-App/todo/urls.py
+++ Projects/Todo-App/todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, todo_create, todo_update,todo_delete
+from .views import home, todo_create, todo_update, todo_delete
 
 urlpatterns = [
     path("", home, name="home"),
